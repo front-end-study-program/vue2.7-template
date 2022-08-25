3fa05e965cae37f65d5f8f91df30aed80efdf9d1
yanquanfahei <1449826851@qq.com>
2022-08-25 23:38:55 +0800

    chore: alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue2'
 import vueJsx from '@vitejs/plugin-vue2-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()]
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    },
+    extensions: ['.vue', '.tsx', '.ts', '.js', '.jsx', '.json']
+  }
 })
